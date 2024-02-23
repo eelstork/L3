56043d75a8b6b572caf997210ba65c80a172781a
Tea <tea.desouza@gmail.com>
2024-02-23 17:32:59 +0800

    Add simple governors

--- Assets/Activ.Util/Runtime/Ext/TransformExt.cs
+++ Assets/Activ.Util/Runtime/Ext/TransformExt.cs
@@ -2,6 +2,7 @@ using InvOp = System.InvalidOperationException;
 using UnityEngine;
 using v3 = UnityEngine.Vector3;
 using T = UnityEngine.Transform;
+using static UnityEngine.Mathf;
 
 namespace Activ.Util{
 public static class TransformExt{
@@ -15,12 +16,6 @@ public static class TransformExt{
         if(!didHit) return 0f;
         var delta = hit.point.y - arg.position.y;
         return delta > maxH ? 0f : delta;
-    }
-
-    public static Transform View(this T self){
-        var view = self.GetChild(0);
-        if(view.name != "View") throw new InvOp($"Not view: {view}");
-        return view;
     }
 
     public static void Clear(this T self){
@@ -37,6 +32,42 @@ public static class TransformExt{
         var u = arg.position - self.position;
         if(planar) u.y = 0f;
         return u.normalized;
+    }
+
+    public static bool HasLOS(this T self, T arg){
+        var P = self.position;
+        var u = arg.position - P;
+        var dist = u.magnitude;
+        var didHit = UnityEngine.Physics.Raycast(P, u, out RaycastHit hit, dist);
+        if(!didHit) return true;
+        // TODO sometimes an object has several colliders
+        if(hit.collider == arg.GetComponent<Collider>()){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public static bool HasLOS(this T self, v3 dir, float dist){
+        var didHit = UnityEngine.Physics.Raycast(
+            self.position, dir, out RaycastHit hit, dist
+        );
+        return !didHit;
+    }
+
+    public static bool IsCoaxial(
+        this T self, T arg, float threshold=5
+    ){
+        var u = arg.Dir(self, planar: true);
+        var A = v3.Angle(u, arg.forward);
+        var B = v3.Angle(u, arg.right);
+        var C = v3.Angle(u, - arg.forward);
+        var D = v3.Angle(u, - arg.right);
+        if(A < threshold) return true;
+        if(B < threshold) return true;
+        if(C < threshold) return true;
+        if(D < threshold) return true;
+        return false;
     }
 
     public static bool IsOn(this T self, T arg){
@@ -66,20 +97,6 @@ public static class TransformExt{
         return t.IsContacting(arg, @unsafe: true);
     }
 
-    public static bool HasLOS(this T self, T arg){
-        var P = self.position;
-        var u = arg.position - P;
-        var dist = u.magnitude;
-        var didHit = UnityEngine.Physics.Raycast(P, u, out RaycastHit hit, dist);
-        if(!didHit) return true;
-        // TODO sometimes an object has several colliders
-        if(hit.collider == arg.GetComponent<Collider>()){
-            return true;
-        }else{
-            return false;
-        }
-    }
-
     public static bool IsNonPhysical(this T self){
         var rb = self.GetComponent<Rigidbody>();
         if(!rb) return true;
@@ -98,6 +115,20 @@ public static class TransformExt{
         if(joint == null) return null;
         return joint.connectedBody.transform;
     }
+
+    public static void LockRot(this T self)
+    => self.Rb().constraints = RigidbodyConstraints.FreezeRotation;
+
+    public static void LockPos(this T self)
+    => self.Rb().constraints = RigidbodyConstraints.FreezePosition;
+
+    public static void LockPosXZ(this T self)
+    => self.Rb().constraints
+        = RigidbodyConstraints.FreezePositionX
+        | RigidbodyConstraints.FreezePositionZ;
+
+    public static void LockY(this T self)
+    => self.Rb().constraints = RigidbodyConstraints.FreezePositionY;
 
     public static bool MoveTo(
         this T self, v3 P, float speed, out bool didFail
@@ -125,21 +156,6 @@ public static class TransformExt{
         }
     }
 
-    public static bool IsCoaxial(
-        this T self, T arg, float threshold=5
-    ){
-        var u = arg.Dir(self, planar: true);
-        var A = v3.Angle(u, arg.forward);
-        var B = v3.Angle(u, arg.right);
-        var C = v3.Angle(u, - arg.forward);
-        var D = v3.Angle(u, - arg.right);
-        if(A < threshold) return true;
-        if(B < threshold) return true;
-        if(C < threshold) return true;
-        if(D < threshold) return true;
-        return false;
-    }
-
     public static v3 NearestCoaxialPosition(
         this T self, T arg, float radius
     ){
@@ -164,19 +180,131 @@ public static class TransformExt{
         return Q;
     }
 
-    public static Vector3 Point(this T self, float x, float y, float z)
-    => self.TransformPoint(new (x, y, z));
+    public static void PMoveAndOrient(  // call in FixedUpdate
+        this Transform self, Transform target, v3 offset,
+        float latency = 0.06f, float speed = 10,
+        float decayDist = 0.2f, float scalar=1f
+    ){
+        PMove(
+            self, target, offset, latency, speed, decayDist,
+            relativePos: false, scalar: scalar
+        );
+        POrient2(self, target);
+    }
+
+    public static float PMove(  // call in FixedUpdate
+        this Transform self, Transform target, v3 offset,
+        float latency=0.06f, float speed=10,
+        float decayDist=0.2f, bool relativePos=true, float scalar=1f
+    ){
+        // Move it
+        v3 P = relativePos ? target.TransformPoint(offset)
+                           : target.position + offset;
+        var u = (P - self.position);
+        var d = u.magnitude;
+        if(d < decayDist){
+            u = u.normalized * speed * d / decayDist;
+        }else{
+            u = u.normalized * speed;
+        }
+        Debug.DrawLine(self.position, P, Color.white);
+        var rb = self.Rb();
+        var v = u - rb.velocity;
+        rb.AddForce(v * scalar * rb.mass / latency);
+        return d;
+    }
+
+    // Axis-align, minimizing effort
+    public static float PAlign(  // call in FixedUpdate
+        this Transform self, Transform target, float latency = 0.2f
+    ){
+        var rb = self.Rb();
+        var u0 = target.ClosestAxis(self.up);
+        var r0 = target.ClosestAxis(self.right);
+        var u = v3.Cross(self.up, u0);
+        var v = v3.Cross(self.right, r0);
+        var w = rb.angularVelocity;
+        u -= w; v -= w;
+        rb.AddTorque((u + v) / 2 * (rb.mass / latency));
+        var a0 = v3.Angle(self.up, u0);
+        var a1 = v3.Angle(self.right, r0);
+        return (a0 + a1) / 2f;
+    }
+
+    static v3 ClosestAxis(this Transform s, v3 u){
+        var v = s.right; var a = v3.Angle(u, s.right);
+        C(-s.right); C(s.up); C(-s.up); C(s.forward); C(-s.forward);
+        return v; void C(v3 w){
+            var b = v3.Angle(u, w); if(b < a){ v = w; a = b; }
+        }
+    }
+
+    public static float POrient2(  // call in FixedUpdate
+        this Transform self, Transform target, float latency = 0.2f
+    ){
+        var rb = self.Rb();
+        var u = v3.Cross(self.up, target.up) / 2;
+        var v = v3.Cross(self.right, target.right) / 2;
+        var w = rb.angularVelocity;
+        u -= w;
+        v -= w;
+        rb.AddTorque((u + v) * rb.mass / latency);
+        var a0 = v3.Angle(self.up, target.up);
+        var a1 = v3.Angle(self.right, target.right);
+        return (a0 + a1) / 2f;
+    }
+
+    // Works like a basic spring; does not cancel existing angular
+    // velocity
+    public static float POrient(  // call in FixedUpdate
+        this Transform self, Transform target, float latency = 0.2f
+    ){
+        var rb = self.Rb();
+        var u = v3.Cross(self.up, target.up) / 2;
+        var v = v3.Cross(self.right, target.right) / 2;
+        rb.AddTorque((u + v) * rb.mass / latency);
+        var a0 = v3.Angle(self.up, target.up);
+        var a1 = v3.Angle(self.right, target.right);
+        return (a0 + a1) / 2f;
+    }
+
+    public static Vector3 Point(
+        this T self, float x, float y, float z
+    ) => self.TransformPoint(new (x, y, z));
 
     public static float Radius(this T self, bool planar){
         var collider = self.GetComponent<Collider>();
         return collider.bounds.Radius(planar);
     }
 
-    public static void SetKinematic(this T self, bool flag=true)
-    => self.Rb().isKinematic = flag;
+    public static C Reachable<C>(
+        this T self, float angle=0f,
+        float h=0.75f, float r=0.5f, float dist=1.6f
+    ){
+        var a = angle * Deg2Rad;
+        var u = new v3(0, -Sin(a), Cos(a));
+        return self.Reachable<C>(dir: u, h, r, dist);
+    }
+
+    public static C Reachable<C>(
+        this T self, v3 dir,
+        float h=0.75f, float r=0.5f, float dist=2.0f
+    ){
+        var P = self.position + v3.up * h + self.forward * r;
+        var u = self.TransformDirection(dir).normalized;
+        var didHit = Physics.Raycast(P, u, out RaycastHit hit, dist);
+        var c = didHit ? hit.collider.GetComponent<C>() : default(C);
+        if(didHit) Debug.DrawLine(
+            P, hit.point, c != null ? Color.green : Color.yellow
+        ); else Debug.DrawRay(P, u * dist, Color.red);
+        return c;
+    }
 
     public static Rigidbody Rb(this T self)
     => self.GetComponent<Rigidbody>();
+
+    public static void SetKinematic(this T self, bool flag=true)
+    => self.Rb().isKinematic = flag;
 
     public static void UnlockAll(this T self)
     => self.Rb().constraints = RigidbodyConstraints.None;
@@ -184,25 +312,10 @@ public static class TransformExt{
     public static void UnlockRot(this T self)
     => self.Rb().constraints = RigidbodyConstraints.FreezePosition;
 
-    public static void LockRot(this T self)
-    => self.Rb().constraints = RigidbodyConstraints.FreezeRotation;
-
-    public static void LockPos(this T self)
-    => self.Rb().constraints = RigidbodyConstraints.FreezePosition;
-
-    public static void LockPosXZ(this T self)
-    => self.Rb().constraints
-        = RigidbodyConstraints.FreezePositionX
-        | RigidbodyConstraints.FreezePositionZ;
-
-    public static void LockY(this T self)
-    => self.Rb().constraints = RigidbodyConstraints.FreezePositionY;
-
-    public static bool HasLOS(this T self, v3 dir, float dist){
-        var didHit = UnityEngine.Physics.Raycast(
-            self.position, dir, out RaycastHit hit, dist
-        );
-        return !didHit;
+    public static Transform View(this T self){
+        var view = self.GetChild(0);
+        if(view.name != "View") throw new InvOp($"Not view: {view}");
+        return view;
     }
 
 }}
